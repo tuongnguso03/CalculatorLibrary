a711fadf61a867b2445bbdc03a300931ce97787f
tuongnguso03 <40336908+tuongnguso03@users.noreply.github.com>
2023-12-27 15:40:31 +0700

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -15,6 +15,6 @@ class TestCalculator:
 
     def test_multiplication(self):
         assert 100 == calculator.multiply(10, 10)
-        
+
     def test_division(self):
         assert 10 == int(calculator.division(10, 1))
